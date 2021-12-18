ee03bf3961079f25ba8f1fdc8cc6736406360667
morkvinaValeria <v.morkvina-2025@kpi.ua>
2021-12-19 00:50:10 +0200

    snake dies

--- public/js/snake.js
+++ public/js/snake.js
@@ -82,6 +82,20 @@ class Snake {
     }
     this.body[0].x = x;
     this.body[0].y = y;
+
+    if (
+      this.body[0].x < 0 ||
+      this.body[0].x >= this.scene.game.config.width ||
+      this.body[0].y < 0 ||
+      this.body[0].y >= this.scene.game.config.height
+    ) {
+      this.scene.scene.restart();
+    }
+
+    let tail = this.body.slice(1);
+    if (tail.some((s) => s.x === this.body[0].x && s.y === this.body[0].y)) {
+      this.scene.scene.restart();
+    }
   }
 }
 
